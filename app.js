6f78860d4de7b7eff8872b4352d69f8393041fc4
Stella40k <bernardstella0704@gmail.com>
2025-06-25 23:30:45 -0300

    feat: conexion de la base de datos

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-//importaciones
-import "dotenv/config";//aca estoy usando env y las activo con config, puedo usar las variables
+//✩ importaciones
+import "dotenv/config";//✩ aca estoy usando env y las activo con config, puedo usar las variables
 import express from "express";
 
 
 
-const app = express(); /* todo lo de express esta guardado en 
+const app = express(); /*✩ todo lo de express esta guardado en 
 app (enrutamiento, esucha, middel) */
 
 const PORT = process.env.PORT;
-//aca digo q el server esccucha tal puerto
+
+app.use(CharacterRoutes)
+
+//✩ aca digo q el server esccucha tal puerto
 app.listen(PORT, () =>{
     console.log(`Se esta escuchando el server en el puerto ${PORT}`)
-})
+});
+
---
+++ src/config/database.js
@@ -0,0 +1,28 @@
+import { Sequelize } from "sequelize";
+import "dotenv/config";
+import { connect } from "http2";
+
+//✩ sequielize es con lo q hare los modelos, al exportar trabajare con tooodo esto:
+export const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST ,
+        dialect: process.env.DB_DIALECT
+    });
+
+export const connect = async() => {
+    try {
+        await sequelize.authenticate();
+        //✩"espera q seque verifique la bd"
+        await sequelize.sync({force:false})
+        //✩ "cada q se agruegen caracteristicas sincroniza los cambios con la bd".
+        // no modifica lo q ya esta, agrega lo q falta, si fuera force: true
+        // borra todo y las hace de 0
+        //✩ comprueba de q se creo bien la bd
+        console.log("Conexion con exito")
+    } catch (error) {
+        console.log("Error en la conexion a la bd :(")
+    }
+}
