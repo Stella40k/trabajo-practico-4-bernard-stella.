38a61ad2d2d2da3ceaaedc9a4a287ddc8f8e6443
Stella40k <bernardstella0704@gmail.com>
2025-06-26 01:30:32 -0300

    fix: renombre .env.example a .env

---
+++ src/controllers/character.controller.js
@@ -0,0 +1,8 @@
+import { Character } from "../models/character.model.js"
+
+export const characterController = []
+
+characterController.create = async (req, res) =>{
+    const{} = req.paramas.
+
+}
