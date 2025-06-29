4ebfff4c1b9cc42f20add200653f0d03175aeef9
Stella40k <bernardstella0704@gmail.com>
2025-06-29 11:09:14 -0300

    feat: estructura de las rutas

--- src/routes/character.routes.js
+++ src/routes/character.routes.js
@@ -1,2 +1,10 @@
 import { Router } from "express";
-import {}
+import { getAllCharacter, createCharacter, deletCharacter,getCharacterById, updateCharacter } from "../controllers/character.controller";
+
+export const routes = Router()
+
+routes.get("/characters", getAllCharacter)
+routes.get("/characters/:id", getCharacterById)
+routes.post("/characters", createCharacter)
+routes.put("/characters/:id", updateCharacter)
+routes.delete("/characters/:id", deletCharacter)
